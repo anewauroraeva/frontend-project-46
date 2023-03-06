ede5ee7dcc22a522bab74853423e78300ad84046
Eugenia <mbp@MacBook-Pro-MBP.Dlink>
2023-03-06 16:47:12 +0300

    add option and arguments to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,7 @@ import { program } from 'commander';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1')
-  .parse();
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
+  
+  program.parse();
