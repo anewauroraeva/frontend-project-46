7af4b0ae7beb6f4f64c612b24d4a0afcf518a162
Eugenia <mbp@MacBook-Pro-MBP.Dlink>
2023-03-13 18:05:06 +0300

    add path.resolve to index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
+/* eslint-disable no-param-reassign */
 import { readFileSync } from 'fs';
+import path from 'path';
 import _ from 'lodash';
 
 const genDiff = (filepath1, filepath2) => {
-  const fileData1 = readFileSync(filepath1, 'utf-8');
-  const fileData2 = readFileSync(filepath2, 'utf-8');
+  const fileData1 = readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8');
+  const fileData2 = readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8');
   const parsedData1 = JSON.parse(fileData1);
   const parsedData2 = JSON.parse(fileData2);
-  console.log(parsedData1);
   const keys1 = Object.keys(parsedData1);
   const keys2 = Object.keys(parsedData2);
-  console.log(keys1);
   const commonKeys = _.union(keys1, keys2);
   const sortedCommonKeys = _.sortBy(commonKeys);
- 
+
   const differences = sortedCommonKeys.reduce((result, key) => {
     const absentKeyInData1 = `- ${key}`;
     const absentKeyInData2 = `+ ${key}`;
@@ -30,23 +30,33 @@ const genDiff = (filepath1, filepath2) => {
     return result;
   }, {});
 
-  console.log(differences);
+  // const differences = sortedCommonKeys.reduce((result, key) => {
+  //   if (!Object.hasOwn(parsedData1, key)) {
+  //     result.push(`+ ${key}: ${parsedData2[key]}`);
+  //   } else if (!Object.hasOwn(parsedData2, key)) {
+  //     result.push(`- ${key}: ${parsedData1[key]}`);
+  //   } else if (parsedData1[key] !== parsedData2[key]) {
+  //     result.push(`- ${key}: ${parsedData1[key]}`);
+  //     result.push(`+ ${key}: ${parsedData2[key]}`);
+  //   } else {
+  //     result.push(`  ${key}: ${parsedData1[key]}`);
+  //   }
+  //   console.log(result);
+  //   const strResult = result.join(',');
+  //   return strResult;
+  // }, []);
 
-  // let strDiff = JSON.stringify(differences);
-  // strDiff.replace('"', '');
-  // console.log(strDiff);
-  // return strDiff;
+  return differences;
 };
 
-/*
-{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}
-*/
+// You need to return:
+// {
+//   - follow: false
+//     host: hexlet.io
+//   - proxy: 123.234.53.22
+//   - timeout: 50
+//   + timeout: 20
+//   + verbose: true
+// }
 
 export default genDiff;
