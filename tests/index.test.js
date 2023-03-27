794ad0f09763ca4ab808e2c8c0611a16c77cb010
Eugenia <mbp@MacBookPro.Dlink>
2023-03-27 17:07:32 +0300

    add filepaths and edit tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,20 +1,25 @@
 // import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import { readFileSync } from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-// const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const filepath1 = getFixturePath('file1.json');
 const filepath2 = getFixturePath('file2.json');
+const filepath3 = getFixturePath('filepath1.yml');
+const filepath4 = getFixturePath('filepath2.yml');
 
-// const jsonFile = readFile('expectedJSON.json');
-const jsonFile = '{\n  - follow: false,\n    host: hexlet.io,\n  - proxy: 123.234.53.22,\n  - timeout: 50,\n  + timeout: 20,\n  + verbose: true\n}';
+const flatFile = readFileSync(path.resolve(process.cwd(), '__fixtures__/expectedFlatDiff.txt'), 'utf-8');
 
-test('JSON files', () => {
-  expect(genDiff(filepath1, filepath2)).toEqual(jsonFile);
+test('JSON flat files', () => {
+  expect(genDiff(filepath1, filepath2)).toEqual(flatFile);
+});
+
+test('YAML flat files', () => {
+  expect(genDiff(filepath3, filepath4)).toEqual(flatFile);
 });
