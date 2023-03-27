b0de3cafc51896ddf716e219385a9fe991f451f6
Eugenia <mbp@MacBookPro.Dlink>
2023-03-27 17:06:21 +0300

    add parser and edit output in index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-/* eslint-disable no-param-reassign */
 import { readFileSync } from 'fs';
 import path from 'path';
 import _ from 'lodash';
+import parseFile from './parsers.js';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2) => { // здесь нужен будет 3ий параметр с форматом вывода
+  console.log(path.resolve(process.cwd(), filepath1));
   const fileData1 = readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8');
   const fileData2 = readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8');
-  const parsedData1 = JSON.parse(fileData1);
-  const parsedData2 = JSON.parse(fileData2);
-  const keys1 = Object.keys(parsedData1);
-  const keys2 = Object.keys(parsedData2);
+  const parcedData1 = parseFile(filepath1, fileData1);
+  const parcedData2 = parseFile(filepath2, fileData2);
+  const keys1 = Object.keys(parcedData1);
+  const keys2 = Object.keys(parcedData2);
   const commonKeys = _.union(keys1, keys2);
   const sortedCommonKeys = _.sortBy(commonKeys);
   const differences = sortedCommonKeys.reduce((result, key) => {
-    if (!Object.hasOwn(parsedData1, key)) {
-      result.splice(result.length, 0, `  + ${key}: ${parsedData2[key]}`);
-    } else if (!Object.hasOwn(parsedData2, key)) {
-      result.splice(result.length, 0, `  - ${key}: ${parsedData1[key]}`);
-    } else if (parsedData1[key] !== parsedData2[key]) {
-      result.splice(result.length, 0, `  - ${key}: ${parsedData1[key]}`);
-      result.splice(result.length, 0, `  + ${key}: ${parsedData2[key]}`);
+    if (!Object.hasOwn(parcedData1, key)) {
+      result.splice(result.length, 0, `  + ${key}: ${parcedData2[key]}`);
+    } else if (!Object.hasOwn(parcedData2, key)) {
+      result.splice(result.length, 0, `  - ${key}: ${parcedData1[key]}`);
+    } else if (parcedData1[key] !== parcedData2[key]) {
+      result.splice(result.length, 0, `  - ${key}: ${parcedData1[key]}`);
+      result.splice(result.length, 0, `  + ${key}: ${parcedData2[key]}`);
     } else {
-      result.splice(result.length, 0, `    ${key}: ${parsedData1[key]}`);
+      result.splice(result.length, 0, `    ${key}: ${parcedData1[key]}`);
     }
     return result;
   }, []);
-  const strDiff = differences.join(',\n');
+  const strDiff = differences.join('\n');
   return `{\n${strDiff}\n}`;
 };
 
