ba045bc476a730ff8f755032327edb9bf7aa69fe
Eugenia <mbp@MacBookPro.Dlink>
2023-04-03 19:25:52 +0300

    add stylish as a default format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ import gendiff from '../src/index.js';
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(gendiff(filepath1, filepath2));
