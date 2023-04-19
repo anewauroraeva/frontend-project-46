ed4bc1289165b25d751a2ec502060c36d744fe65
Eugenia <mbp@MacBookPro.Dlink>
2023-04-19 19:45:28 +0300

    edit output and add formatting

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import parseFile from './parsers.js';
 import buildDiff from './buildDiff.js';
-import stylish from './formatters/stylish.js';
-import plain from './formatters/plain.js';
+import format from './formatters/index.js';
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const fileData1 = readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8');
   const fileData2 = readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8');
   const parcedData1 = parseFile(filepath1, fileData1);
   const parcedData2 = parseFile(filepath2, fileData2);
 
-  return plain(buildDiff(parcedData1, parcedData2));
+  const diff = buildDiff(parcedData1, parcedData2);
+  return format(diff, formatName);
 };
 
 export default genDiff;
