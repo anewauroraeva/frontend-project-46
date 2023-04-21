babcf13c9e4e31d13b3186ae4ddc954ac6f7cc9b
Eugenia <mbp@MacBookPro.Dlink>
2023-04-21 20:21:02 +0300

    add options to commander

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 
-import { program } from 'commander';
+import { program, Option } from 'commander';
 import gendiff from '../src/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1')
-  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    const diff = gendiff(filepath1, filepath2, program.format);
+  .addOption(new Option('-f, --format <type>', 'output format')
+    .choices(['stylish', 'plain', 'json']).default('stylish'))
+  .action((filepath1, filepath2, option) => {
+    const diff = gendiff(filepath1, filepath2, option.format);
     console.log(diff);
   });
 
