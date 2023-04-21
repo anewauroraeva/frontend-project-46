3e309217a062623fa367ed2a8e8d827e1b342dd2
Eugenia <mbp@MacBookPro.Dlink>
2023-04-21 20:27:43 +0300

    add json formatter to format choosing func

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,11 @@
 import stylish from './stylish.js';
 import plain from './plain.js';
+import json from './json.js';
 
 const format = (diff, formatName = 'stylish') => {
-  if (formatName === 'plain') {
-    return plain(diff);
-  }
+  if (formatName === 'plain') return plain(diff);
+  if (formatName === 'json') return json(diff);
+
   return stylish(diff);
 };
 
