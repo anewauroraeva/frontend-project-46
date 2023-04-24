05465214855ca96a3a4dad477b8c56006ba08d1c
Eugenia <mbp@MacBookPro.Dlink>
2023-04-24 13:13:40 +0300

    edit stringify func, depths in stylish func, delete comments

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,19 +1,15 @@
 /* eslint-disable max-len */
 import _ from 'lodash';
 
-const stringify = (value, replacer = '    ') => {
-  const iter = (currentValue, depth) => {
-    if (!_.isObject(currentValue)) {
-      return `${currentValue}`;
-    }
-    const keyIndent = replacer.repeat(depth + 1);
-    const bracketIndent = replacer.repeat(depth);
-    const lines = Object.entries(currentValue)
-      .map(([key, val]) => `${keyIndent}${key}: ${iter(val, depth + 1)}`);
-    return ['{', ...lines, `${bracketIndent}}`].join('\n');
-  };
-
-  return iter(value, 1);
+const stringify = (value, depth, replacer = '    ') => {
+  if (!_.isObject(value)) {
+    return `${value}`;
+  }
+  const keyIndent = replacer.repeat(depth + 1);
+  const bracketIndent = replacer.repeat(depth);
+  const lines = Object.entries(value)
+    .map(([key, val]) => `${keyIndent}${key}: ${stringify(val, depth + 1)}`);
+  return ['{', ...lines, `${bracketIndent}}`].join('\n');
 };
 
 const stylish = (diff) => {
@@ -23,70 +19,22 @@ const stylish = (diff) => {
     const bracketIndent = replacer.repeat(depth + 1);
     switch (node.type) {
       case 'added':
-        return `${indent}  + ${node.key}: ${stringify(node.value)}`;
+        return `${indent}  + ${node.key}: ${stringify(node.value, depth + 1)}`;
       case 'deleted':
-        return `${indent}  - ${node.key}: ${stringify(node.value)}`;
+        return `${indent}  - ${node.key}: ${stringify(node.value, depth + 1)}`;
       case 'nested':
-        return `${indent}    ${node.key}: ${['{', ...iter(node.value, depth + 1), `${bracketIndent}}`].join('\n')}`;
+        return `${indent}    ${node.key}: ${['{', ...iter(node.children, depth + 1), `${bracketIndent}}`].join('\n')}`;
       case 'unchanged':
-        return `${indent}    ${node.key}: ${stringify(node.value)}`;
+        return `${indent}    ${node.key}: ${stringify(node.value, depth)}`;
       case 'changed':
-        return `${indent}  - ${node.key}: ${stringify(node.value)}\n${indent}  + ${node.key}: ${stringify(node.value2)}`;
+        return `${indent}  - ${node.key}: ${stringify(node.oldValue, depth + 1)}\n${indent}  + ${node.key}: ${stringify(node.newValue, depth + 1)}`;
       default:
-        return 'OOPS';
+        throw new Error('Oops, something went wrong!');
     }
   });
 
   const stylishDiff = iter(diff, 0);
   return ['{', ...stylishDiff, '}'].join('\n');
 };
-
-/*
-const space = ' ';
-const spacesCount = 4;
-const spacesToLeft = 2;
-
-const unchangedKeyIndent = (depth) => space.repeat(depth * spacesCount);
-const changedKeyIndent = (depth) => space.repeat(depth * spacesCount - spacesToLeft);
-const bracketIndent = (depth) => space.repeat(depth * spacesCount - spacesCount);
-
-const stringify = (value) => {
-  const iter = (currentValue, depth) => {
-    if (!_.isObject(currentValue)) {
-      return `${currentValue}`;
-    }
-
-    const lines = Object.entries(currentValue)
-      .map(([key, val]) => `${unchangedKeyIndent(depth)}${key}: ${iter(val, depth + 1)}`);
-    return ['{', ...lines, `${bracketIndent(depth)}}`].join('\n');
-  };
-
-  return iter(value, 2);
-};
-
-const stylish = (diff) => {
-  const iter = (tree, depth) => tree.map((node) => {
-    switch (node.type) {
-      case 'added':
-        console.log(stringify(node.value));
-        return `${changedKeyIndent(depth)}+ ${node.key}: ${stringify(node.value)}`;
-      case 'deleted':
-        console.log(stringify(node.value));
-        return `${changedKeyIndent(depth)}- ${node.key}: ${stringify(node.value)}`;
-      case 'nested':
-        return `${unchangedKeyIndent(depth)}${node.key}: ${['{', ...iter(node.value, depth + 1), `${bracketIndent(depth)}}`].join('\n')}`;
-      case 'unchanged':
-        return `${unchangedKeyIndent(depth)}${node.key}: ${stringify(node.value)}`;
-      case 'changed':
-        return `${changedKeyIndent(depth)}- ${node.key}: ${stringify(node.value)}\n${changedKeyIndent(depth)}+ ${node.key}: ${stringify(node.value2)}`;
-      default:
-        return 'OOPS';
-    }
-  });
-
-  const stylishDiff = iter(diff, 1);
-  return ['{', ...stylishDiff, '}'].join('\n');
-};
-*/
 
 export default stylish;
