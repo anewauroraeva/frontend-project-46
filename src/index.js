0701cb4f8a214e6a3f0ea8645a8cf9b93eafe232
Eugenia <mbp@MacBookPro.Dlink>
2023-05-02 16:55:19 +0300

    add extention reception

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import format from './formatters/index.js';
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const fileData1 = readFileSync(path.resolve(process.cwd(), filepath1), 'utf-8');
   const fileData2 = readFileSync(path.resolve(process.cwd(), filepath2), 'utf-8');
-  const parcedData1 = parseFile(filepath1, fileData1);
-  const parcedData2 = parseFile(filepath2, fileData2);
+  const fileExt1 = path.extname(filepath1);
+  const fileExt2 = path.extname(filepath2);
+  const parcedData1 = parseFile(fileData1, fileExt1);
+  const parcedData2 = parseFile(fileData2, fileExt2);
 
   const diff = buildDiff(parcedData1, parcedData2);
   return format(diff, formatName);
