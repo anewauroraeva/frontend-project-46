f8ee6448eeb1b1b81fa08a84de987c62d59834c2
Eugenia <mbp@MacBookPro.Dlink>
2023-05-02 16:52:10 +0300

    split end return

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -12,9 +12,12 @@ const buildDiff = (data1, data2) => {
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
       return { key, type: 'nested', children: buildDiff(data1[key], data2[key]) };
     }
-    return data1[key] === data2[key] ? { key, type: 'unchanged', value: data1[key] } : {
-      key, type: 'changed', oldValue: data1[key], newValue: data2[key],
-    };
+    if (data1[key] !== data2[key]) {
+      return {
+        key, type: 'changed', oldValue: data1[key], newValue: data2[key],
+      };
+    }
+    return { key, type: 'unchanged', value: data1[key] };
   });
 
   return diff;
